64249f60c4a9a29efef2f7bdf5ffb869b83237ef
lv-paopao <53527837+lv-paopao@users.noreply.github.com>
2019-08-18 23:08:06 +0800

    Update 计算等差数列值.c

--- 8-18/计算等差数列值.c
+++ 8-18/计算等差数列值.c
@@ -1,25 +1,25 @@
-//#define _CRT_SECURE_NO_WARNINGS 1
-//#include<stdio.h>
-//#include<stdlib.h>
-//int main()
-//{
-//	int i;
-//	double sum;
-//	double sum1 = 0;
-//	double sum2 = 0;
-//	for (i = 1; i < 100; i+=2)//¼ÆËãÆæÊýºÍ
-//	{
-//		sum1 = sum1 + 1.0 / i;
-//	}
-//	for (i = 2; i <= 100; i += 2)//¼ÆËãÅ¼ÊýºÍ
-//	{
-//		sum2 = sum2 + 1.0 / i;
-//	}
-//	sum = sum1 - sum2;
-//	printf("sum=%f\n", sum);
-//	system("pause");
-//	return 0;
-//}
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int i;
+	double sum;
+	double sum1 = 0;
+	double sum2 = 0;
+	for (i = 1; i < 100; i+=2)//Â¼Ã†Ã‹Ã£Ã†Ã¦ÃŠÃ½ÂºÃ
+	{
+		sum1 = sum1 + 1.0 / i;
+	}
+	for (i = 2; i <= 100; i += 2)//Â¼Ã†Ã‹Ã£Ã…Â¼ÃŠÃ½ÂºÃ
+	{
+		sum2 = sum2 + 1.0 / i;
+	}
+	sum = sum1 - sum2;
+	printf("sum=%f\n", sum);
+	system("pause");
+	return 0;
+}
 
 //1/1+1/3+1/5+...+1/99(sum1)
 //-
